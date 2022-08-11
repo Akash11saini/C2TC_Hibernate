7a7f38090fae4741a0e4f7e9e807b27fdc55c93a
AKASH SAINI <AKASH SAINI@LAPTOP-7MBMJ6H3>
2022-08-11 18:28:42 +0530

    first commit

--- AcharyaJPACrudOperation/src/com/cg/client/Client.java
+++ AcharyaJPACrudOperation/src/com/cg/client/Client.java
@@ -7,37 +7,36 @@ import com.cg.service.StudentServiceImpl;
 public class Client {
 
 	public static void main(String[] args) {
-		
+
 		Student student = new Student();
 		StudentService service = new StudentServiceImpl();
 		
-		//Create Operation
-		student.setId(100);
-		student.setName("Akash");
-		service.addStudent(student);
-		System.out.println("Data is added");
-		
-		//Retrieve
-		student = service.findStudentById(100);
-		System.out.println("ID is: " + student.getId());
-		System.out.println("Name is: " + student.getName());
-		
-		//Update
+		  //Create Operation 
+		student.setId(100); student.setName("Akash");
+		  service.addStudent(student); System.out.println("Data is added");
+		  
+		  
+		  //Retrieve 
+		  student = service.findStudentById(100);
+		  System.out.println("ID is: " + student.getId());
+		  System.out.println("Name is: " + student.getName());
+		 
+
+		// Update
 		student = service.findStudentById(100);
 		student.setName("Akash Saini");
 		service.updateStudent(student);
 		System.out.println("Update is Successfull");
-		
-		student = service.findStudentById(100);
-		System.out.println("ID is: " + student.getId());
-		System.out.println("Name is: " + student.getName());
-		
-		//Delete
-		student = service.findStudentById(100);
-		service.removeStudent(student);
-		System.out.println("Deleted successfully");
-		
 
+		
+		  student = service.findStudentById(100); System.out.println("ID is: " +
+		  student.getId()); System.out.println("Name is: " + student.getName());
+		  
+		  //Delete 
+		  student = service.findStudentById(100);
+		  service.removeStudent(student); System.out.println("Deleted successfully");
+		  
+		 
 	}
 
 }
